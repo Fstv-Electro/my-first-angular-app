a12f07972ca9322781fc553f0fe70b082a3dedd6
Oleksandr Kutsenko <104622826+Fstv-Electro@users.noreply.github.com>
2023-10-22 18:17:46 +0300

    fix: fixed bug 8202

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-
 
 import { AppComponent } from './app.component';
 import { PasswordInputComponent } from './password-input/password-input.component';
@@ -19,8 +16,6 @@ import { StrengthBarComponent } from './strength-bar/strength-bar.component';
     BrowserModule,
     ReactiveFormsModule,
     FormsModule,
-    MatInputModule,
-    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/password-input/password-input.component.ts
+++ src/app/password-input/password-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, forwardRef } from '@angular/core';
+import { Component, OnDestroy, forwardRef, Input } from '@angular/core';
 import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { Subscription } from 'rxjs';
 
